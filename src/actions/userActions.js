421c8610ede3986c044bf7545574c057a3197187
GroverTB <grover.jtb@gmail.com>
2018-04-17 00:15:00 -0500

    [UPDATE] structure redux

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -22,5 +22,18 @@ export function getUsers() {
     )
   }
 }
-  
 
+// import axios from 'axios'
+
+// export const SHOW_USERS = 'SHOW_USERS'
+
+// export function showUsers() {
+    
+//     return (dispatch, getState) => {
+//         axios.get('http://jsonplaceholder.typicode.com/users')
+//             .then((response) => {
+//                 dispatch( { type: SHOW_USERS, payload: response.data } ) 
+//             }) 
+//     }
+    
+// }
